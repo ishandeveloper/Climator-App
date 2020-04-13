867eddd2d9db1c97f9675064a1ed1eb197aa6afd
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-13 23:20:09 +0530

    Constants

---
+++ lib/constants.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+const TextStyle bigLocation = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 32,
+  fontWeight: FontWeight.w600,
+);
+const TextStyle bigTemperature = TextStyle(
+  fontFamily: 'Poppins',
+  fontSize: 36,
+  fontWeight: FontWeight.w500,
+);
