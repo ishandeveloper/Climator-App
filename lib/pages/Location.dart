3abe790113906da942dd2a5133fcd712b6289f73
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-13 23:20:31 +0530

    Find Location

---
+++ lib/pages/Location.dart
@@ -0,0 +1,70 @@
+import 'package:climate_app/constants.dart';
+import 'package:flutter/material.dart';
+
+class ClimateLocation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/images/background.png'),
+              fit: BoxFit.cover)),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: Icon(Icons.location_city,
+            color: Colors.grey[900],
+          ),
+          // title: Text('Hi'),
+          backgroundColor: Color.fromARGB(1000, 170, 255, 236),
+          elevation: 0,
+          actions: <Widget>[
+            Container(
+              margin: EdgeInsets.only(right: 10),
+              child: GestureDetector(
+                child: Icon(
+                  Icons.info,
+                  color: Colors.grey[900],
+                ),
+              ),
+            )
+          ],
+        ),
+        backgroundColor: Colors.transparent,
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.only(
+              top: 80,
+              left: 30,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  'Find your location',
+                  style: bigLocation.copyWith(fontSize: 30),
+                ),
+                Container(
+                  margin:EdgeInsets.only(top:15),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:BorderRadius.circular(30)
+                  ),
+                  padding: EdgeInsets.only(left:10),
+                  width: MediaQuery.of(context).size.width * 0.80,
+                  child: TextField(
+                    autofocus: false,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.search),
+                        border: InputBorder.none,
+                        // helperText: 'Search For Any City',
+                        hintText: 'Search for any city'),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
