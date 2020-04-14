3756665107414dfbc4561c6e316d45fc97620c92
Ishan Sharma <ishandeveloper@outlook.com>
2020-04-14 13:45:36 +0530

    TextTheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,31 @@
 import 'package:climate_app/pages/CimateHome.dart';
 import 'package:climate_app/pages/Location.dart';
+import 'package:climate_app/pages/about.dart';
 import 'package:flutter/material.dart';
 
 // import 'pages/CimateHome.dart';
 
-void main()=>runApp(ClimateApp());
+void main() => runApp(ClimateApp());
 
 class ClimateApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Climate',
-      theme: ThemeData(iconTheme: IconThemeData(color: Colors.black)),
+      theme: ThemeData(
+        iconTheme: IconThemeData(color: Colors.black),
+        textTheme: TextTheme(
+          body1: TextStyle(
+            fontFamily: 'Poppins',
+          ),
+        ),
+      ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      initialRoute: '/about',
       routes: {
-        '/':(context)=>ClimateHome(),
-        '/locator':(context)=>ClimateLocation(),
+        '/': (context) => ClimateHome(),
+        '/locator': (context) => ClimateLocation(),
+        '/about': (context) => AboutPage()
       },
     );
   }
